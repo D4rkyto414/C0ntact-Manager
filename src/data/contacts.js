6ad2466e289fdbad68d562008555e7d34630b053
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-02 22:18:51 -0500

    Delete src/data directory

--- src/data/contacts.js
+++
@@ -1,5 +0,0 @@
-export const contactsData = [
-  { id: 1, name: "Juan Pérez", phone: "987654321", favorite: false },
-  { id: 2, name: "María López", phone: "912345678", favorite: true },
-  { id: 3, name: "Carlos Ruiz", phone: "956789012", favorite: false }
-];
